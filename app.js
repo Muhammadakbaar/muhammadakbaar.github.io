9c21080adf40e2813024a2dff05e426efec7ba89
muhammadakbaar <mohammadakbaar11@gmail.com>
2024-06-25 21:01:00 +0700

    add servece worker

--- app.js
+++ app.js
@@ -64,3 +64,15 @@ fetch('manifest.json')
     console.error('Gagal mengambil manifest:', error);
   });
 
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('service-worker.js')
+            .then(registration => {
+                console.log('Service Worker registered with scope:', registration.scope);
+            })
+            .catch(error => {
+                console.error('Service Worker registration failed:', error);
+            });
+    });
+}
+
