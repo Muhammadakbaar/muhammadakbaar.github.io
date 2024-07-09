91569639a2babe6a7c2de27b7d66a22e0d5f0ce0
muhammadakbaar <mohammadakbaar11@gmail.com>
2024-07-09 19:50:35 +0700

    add servece worker

--- service-worker.js
+++ service-worker.js
@@ -1,48 +1,69 @@
-const CACHE_NAME = 'my-portfolio-cache-v1';
+const CACHE_NAME = 'portfolio-app-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/styles/styles.css',
-    '/js/app.js',
-    '/icons/icons8-son-goku-48.png',
-    '/icons/icons8-son-goku-72.png',
-    '/icons/icons8-son-goku-96.png',
-    '/icons/icons8-son-goku-128.png',
-    '/icons/icons8-son-goku-192.png',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css',
-    'https://fonts.googleapis.com/css2?family=Poppins&display=swap',
-    'https://fonts.gstatic.com/s/poppins/v15/pxiEyp8kv8JHgFVrJJfedw.ttf', // example font URL
-    'https://raw.githubusercontent.com/Muhammadakbaar/Some-Picture/main/654244.jpg'
+  '/',
+  '/index.html',
+  '/styles/styles.css',
+  '/app.js',
+  '/img/hero1.png',
+  '/icons/icons8-son-goku-48.png',
+  '/icons/icons8-son-goku-72.png',
+  '/icons/icons8-son-goku-96.png',
+  '/icons/icons8-son-goku-128.png',
+  '/icons/icons8-son-goku-192.png',
+  '/manifest.json',
+  '/cv.pdf',
+  '/img/port1.png',
+  '/img/port2.svg',
+  '/img/port3.png',
+  '/img/port4.jpg',
+  '/img/port5.png',
+  '/img/port6.png',
+  '/styles/_media.scss',
+  '/styles/styles.css',
+  '/styles/styles.css.map',
+  '/styles/styles.scss',
 ];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-            .then(() => self.skipWaiting())
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+      .catch(error => {
+        console.error('Failed to cache files during install:', error);
+      })
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.filter(cacheName => cacheName !== CACHE_NAME)
-                    .map(cacheName => caches.delete(cacheName))
-            );
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('Deleting old cache:', cache);
+            return caches.delete(cache);
+          }
         })
-    );
-    self.clients.claim();
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).catch(error => {
+          console.error('Fetch failed:', error);
+          throw error;
+        });
+      })
+  );
 });
